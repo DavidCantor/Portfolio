10228c17164b9ecdb5fbd85b4b650ef53bfc0ebf
David Cantor <dsantiagocanvar@gmail.com>
2025-12-22 13:49:19 -0500

    Configure Nuxt SSR for Netlify deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,8 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   ssr: true,
   modules: ['@nuxtjs/tailwindcss'],
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  nitro: {
+    preset: 'netlify'
+  }
 })
